6cabb93425d1a307d30993f7ed1a0f1b113048d4
rohira-dhruv <rohirajdhruv@gmail.com>
2022-10-12 15:09:45 +0530

    Create basic UI and basic functionality

--- app/src/main/java/androidsamples/java/dicegames/Die6.java
+++ app/src/main/java/androidsamples/java/dicegames/Die6.java
@@ -6,18 +6,25 @@ import java.util.Random;
  * An implementation of a six-faced {@link Die} using {@link Random}.
  */
 public class Die6 implements Die {
+
+  private static final int FACES = 6;
+  private Random mRnd;
+  private int mValue;
+
   public Die6() {
     // TODO implement method
+    mRnd =  new Random();
   }
 
   @Override
   public void roll() {
     // TODO implement method
+    mValue =  mRnd.nextInt(FACES) + 1;
   }
 
   @Override
   public int value() {
     // TODO implement method
-    return 0;
+    return mValue;
   }
 }
