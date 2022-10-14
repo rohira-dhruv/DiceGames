6ae68637abdec00ec4f5f7aadce781fe4f78f26f
rohira-dhruv <rohirajdhruv@gmail.com>
2022-10-14 16:39:31 +0530

    added instrumented test for orientation

--- app/src/androidTest/java/androidsamples/java/dicegames/WalletInstrumentedTest.java
+++ app/src/androidTest/java/androidsamples/java/dicegames/WalletInstrumentedTest.java
@@ -10,19 +10,26 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.runner.lifecycle.Stage.RESUMED;
 import static org.junit.Assert.assertEquals;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.RemoteException;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import androidx.test.runner.lifecycle.Stage;
 import androidx.test.uiautomator.UiDevice;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Collection;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -46,6 +53,27 @@ public class WalletInstrumentedTest {
   public void total_roll_count_increment() {
     onView(withId(R.id.btn_die)).perform(click());
     onView(withId(R.id.txt_total_rolls)).check(matches(withText("1")));
+  }
+
+  public static Activity getCurrentActivity() {
+    final Activity[] currentActivity = {null};
+    getInstrumentation().runOnMainSync(new Runnable() {
+      @Override
+      public void run() {
+        Collection resumedActivities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(RESUMED);
+        if(resumedActivities.iterator().hasNext()){
+          currentActivity[0] = (Activity)resumedActivities.iterator().next();
+        }
+      }
+    });
+    return currentActivity[0];
+  }
+
+  @Test
+  public void checkOrientation() {
+    Activity mActivity = getCurrentActivity();
+    mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
   }
 
 
